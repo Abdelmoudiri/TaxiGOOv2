210369162323413180267d71c84fe45aa8d84013
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-03-06 11:18:02 +0000

    finaliser la notification par email

--- app/Mail/LoginNotificationMail.php
+++ app/Mail/LoginNotificationMail.php
@@ -41,7 +41,7 @@ class LoginNotificationMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.login-notification',
         );
     }
 
--- resources/views/emails/login-notification.blade.php
+++ resources/views/emails/login-notification.blade.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
- <title>Login Notification</title>
+    <title>Login Notification</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            color: #333;
+        }
+        h1 {
+            color: #4CAF50;
+        }
+    </style>
 </head>
 <body>
- <h1>Login Notification</h1>
- <p>Hello {{ $userName }},</p>
- <p>You have successfully logged in to your account.</p>
- <p>Login Time: {{ $loginTime }}</p>
- <p>If this was not you, please contact support.</p>
+    <h1>Login Notification</h1>
+    <p>Hello {{ $userName }},</p>
+    <p>You have successfully logged in to your account.</p>
+    <p>Login Time: {{ $loginTime }}</p>
+    <p>If this was not you, please contact support immediately.</p>
 </body>
 </html>
